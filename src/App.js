650ee334b8517a0a529d88527eddc98194da04f6
Liz Lee <liz.lee@winningappliances.com.au>
2022-03-31 21:29:40 +1100

    more updates to home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import headshot from './images/Liz_Lee_Headshot.jpg';
+import vine from './images/Vine.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <img src={vine} className="vine" alt="decorative vine" />
+      <header className="header">
+        <span className="headshot">
+          <img src={headshot}  alt="Liz Lee Headshot" />
+        </span>
+        <h1>Liz Lee</h1>
+        <div class="details">
+          <p>Lead Front End Engineer</p>
+          <p>Engieering Mangement</p>
+          <p>JavaScript / HTML / CSS</p>
+          <p>React / Angular</p>
+          <p>DevOps</p>
+          <p>UI Design Systems</p>
+        </div>
       </header>
+      <section className="content">
+        <h2>
+          Lets Build<br />Great Products
+        </h2>
+        <nav class="nav">
+          <ul>
+            <li><a href="https://www.linkedin.com/in/lizluxlee/" target="_blank" rel="noreferrer">LinkedIn</a></li>
+            <li><a href="https://github.com/lizlux" target="_blank" rel="noreferrer">Github</a></li>
+            <li><a href="/Liz_Lee_Resume.pdf" target="_blank" rel="noreferrer">Resume</a></li>
+          </ul>
+        </nav>
+      </section>
     </div>
   );
 }
