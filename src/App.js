20cdc946f52391c94370e4f6e2b8962f565167eb
Liz Lee <liz.lee@winningappliances.com.au>
2022-04-16 10:17:27 +1000

    running prettier on entire app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import headshot from './images/Liz_Lee_Headshot.jpg';
-import vine from './images/Vine.svg';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
+import headshot from "./images/Liz_Lee_Headshot.jpg";
+import vine from "./images/Vine.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
@@ -14,7 +9,7 @@ function App() {
       <img src={vine} className="vine" alt="decorative vine" />
       <header className="header">
         <span className="headshot">
-          <img src={headshot}  alt="Liz Lee Headshot" />
+          <img src={headshot} alt="Liz Lee Headshot" />
         </span>
         <h1>Liz Lee</h1>
         <div class="details">
@@ -28,13 +23,35 @@ function App() {
       </header>
       <section className="content">
         <h2>
-          Lets Build<br />Great Products
+          Lets Build
+          <br />
+          Great Products
         </h2>
         <nav class="nav">
           <ul>
-            <li><a href="https://www.linkedin.com/in/lizluxlee/" target="_blank" rel="noreferrer">LinkedIn</a></li>
-            <li><a href="https://github.com/lizlux" target="_blank" rel="noreferrer">Github</a></li>
-            <li><a href="/Liz_Lee_Resume.pdf" target="_blank" rel="noreferrer">Resume</a></li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/lizluxlee/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/lizlux"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+            </li>
+            <li>
+              <a href="/Liz_Lee_Resume.pdf" target="_blank" rel="noreferrer">
+                Resume
+              </a>
+            </li>
           </ul>
         </nav>
       </section>
